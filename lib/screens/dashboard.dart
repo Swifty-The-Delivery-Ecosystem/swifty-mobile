b385d361a5c138852e5eed17718946b9d9a9e037
Aditya062003 <adityavinay@iitbhilai.ac.in>
2024-01-01 18:12:28 +0530

    feat: ui for home screen

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -129,3 +129,36 @@ class _DashboardState extends State<Dashboard> {
     );
   }
 }
+// else {
+//                     return  Center(child: Shimmer.fromColors(
+//                     baseColor: Colors.grey.shade300,
+//                     highlightColor: Colors.grey.shade100,
+//                         enabled: true,
+//                         child: const SingleChildScrollView(
+//                         physics: NeverScrollableScrollPhysics(),
+//                   child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   mainAxisSize: MainAxisSize.max,
+//                   children: [
+//                   BannerPlaceholder(),
+//                   TitlePlaceholder(width: double.infinity),
+//                   SizedBox(height: 16.0),
+//                   ContentPlaceholder(
+//                   lineType: ContentLineType.threeLines,
+//                   ),
+//                   SizedBox(height: 16.0),
+//                   TitlePlaceholder(width: 200.0),
+//                   SizedBox(height: 16.0),
+//                   ContentPlaceholder(
+//                   lineType: ContentLineType.twoLines,
+//                   ),
+//                   SizedBox(height: 16.0),
+//                   TitlePlaceholder(width: 200.0),
+//                   SizedBox(height: 16.0),
+//                   ContentPlaceholder(
+//                   lineType: ContentLineType.twoLines,
+//                   ),
+//                   ],
+//                   ),
+//                   )));
+//                   }
